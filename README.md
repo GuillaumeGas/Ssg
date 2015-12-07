578003d783fe34a3da5d22298540db964c654c74
Guillaume Gas <guillaume@gas28.net>
2015-12-07 16:00:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ssg
+===
+
+A Symfony project created on December 7, 2015, 3:54 pm.
---
+++ src/SSG/NewsBundle/SSGNewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SSG\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SSGNewsBundle extends Bundle
+{
+}
